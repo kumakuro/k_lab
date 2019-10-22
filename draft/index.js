ee65034e226f1eb329e651a52ab775e4e61e1a8f
Peggy <maggiegu94@163.com>
2019-10-22 09:52:33 +0800

    add

--- draft/index.js
+++ draft/index.js
@@ -1,20 +1,75 @@
-var isPowerOfTwo = function (n) {
-  let flag = true, result = 0, prev = 0;
-  for (let i = 0; flag; i++) {
-    if (i === 0) {
-      result = 1;
-    }
-    if (i === 1) {
-      result = 2;
-    }
-    if (i > 1) {
-      result = result * 2;
-    }
-    if (result > n) {
-      flag = false
-    }
-    if (result === n) {
-      break
+var toLowerCase = function (str) {
+  let upStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    lowerStr = 'abcdefghijklmnopqrstuvwxyz',
+    strArr = str.split('');
+
+  for (let i in strArr) {
+    let idx = upStr.indexOf(strArr[i])
+    if (idx > -1) {
+      strArr[i] = lowerStr[idx]
     }
   }
+
+  return strArr.join('')
 };
+
+console.log(toLowerCase('DjhsIDHknsD'))
+
+
+// var removeOuterParentheses = function (S) {
+//   let deleteArr = [],
+//     sArr = S.split(''),
+//     count = 0;
+
+//   for (let i = 0; i < sArr.length; i++) {
+//     if (sArr[i] === '(') {
+//       count++;
+//     } else {
+//       count--;
+//     }
+
+//     if (sArr[i] === '(') {
+//       if (count === 1) {
+//         deleteArr.push(i)
+//       }
+//     }
+//     if (sArr[i] === ')') {
+//       if (count === 0) {
+//         deleteArr.push(i)
+//       }
+//     }
+//   }
+//   deleteArr.reverse()
+//   for (let j of deleteArr) {
+//     sArr.splice(j, 1)
+//   }
+
+//   return sArr.join('')
+// };
+
+// let str = "(()(()))()"
+// console.log('answer --- > ', removeOuterParentheses(str))
+
+
+
+
+// var isPowerOfTwo = function (n) {
+//   let flag = true, result = 0, prev = 0;
+//   for (let i = 0; flag; i++) {
+//     if (i === 0) {
+//       result = 1;
+//     }
+//     if (i === 1) {
+//       result = 2;
+//     }
+//     if (i > 1) {
+//       result = result * 2;
+//     }
+//     if (result > n) {
+//       flag = false
+//     }
+//     if (result === n) {
+//       break
+//     }
+//   }
+// };
