eea0b83ef031fcde02a97b1daff748a9683f025e
Peggy <maggiegu94@163.com>
2019-10-28 11:55:28 +0800

    add

--- exTest/public/javascripts/tools/choose.js
+++ exTest/public/javascripts/tools/choose.js
@@ -1,26 +1,62 @@
 $(document).ready(function () {
+
+  let str = "  Wow! It wasn't just a *crack* in the rock. It was a secret *entrance to* an *ancient*\n*room*!";
+
   $(document).on('click', '#handle', function () {
-    let contentArr = $('#input').val().replace(/",(\s)+"/g, ' ').replace(/\\n/g, ' ').split(' '),
-      contentHtml = '',
-      markStr = '';
+    let inputStr = $('#input').val();
+    if (inputStr.length > 0) {
+      // let contentArr = inputStr.replace(/",(\s)+"/g, ' ').replace(/\\n/g, ' ').split(' '),
+      let contentArr = inputStr.replace(/\\n/g, ' ').split(' '),
+        contentHtml = '',
+        markArr = [];
 
-    for (let i in contentArr) {
-      let fragment = '';
-      if (contentArr[i].indexOf('{{') > -1 || contentArr[i].indexOf('}}') > -1) {
-        fragment = `<div class="item active">${contentArr[i]}</div>`
-        markStr += '[' + i + '],'
-      } else {
-        fragment = `<div class="item">${contentArr[i]}</div>`
+      for (let i in contentArr) {
+        let fragment = '';
+        if (contentArr[i].indexOf('*') > -1) {
+          fragment = `<div class="item active">${contentArr[i]}</div>`;
+          markArr.push(contentArr[i])
+        } else {
+          fragment = `<div class="item">${contentArr[i]}</div>`
+        }
+        contentHtml += fragment;
       }
-
-      contentHtml += fragment;
+      $(document).find('.box').html(contentHtml + `<div class="clear"></div>`);
+      wordsHandle(markArr);
     }
 
-    $(document).find('.box').append(contentHtml);
-    $(document).find('.result').html(markStr);
   })
 
   $('.box').on('click', 'item', function () {
 
   })
 })
+
+function wordsHandle(inputArr) {
+  let wordArr = [],
+    reg1 = /\*(\w+)\*/g,
+    reg2 = /\*/g,
+    backArr = [],
+    backStr = '';
+
+  for (let i in inputArr) {
+    let temp = inputArr[i].match(reg1, '$1');
+    if (temp) {
+      if (wordArr.length > 0) {
+        backArr.push(wordArr.join(' '));
+        wordArr = [];
+      }
+      backArr.push(temp[0])
+    } else {
+      if (inputArr[i].indexOf('*') === 0 && wordArr.length > 0) {
+        backArr.push(wordArr.join(' '));
+        wordArr = [];
+      }
+      wordArr.push(inputArr[i])
+    }
+  }
+
+  for (let i in backArr) {
+    backStr += '"' + backArr[i].replace(reg2, '') + '"' + (i * 1 === backArr.length - 1 ? '' : ',');
+  }
+  $(document).find('.result').val(backStr);
+}
--- exTest/routes/index.js
+++ exTest/routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/find', function(req, res, next) {
+  res.render('findtheword2', { title: 'findtheword2!!!' });
+});
+
+router.get('/tools', function(req, res, next) {
+  res.render('tools', { title: '工坊主' });
+});
+
 module.exports = router;
