7774d9973ac8ae7387fa8e6977b64cb3bdcfd376
Peggy <maggiegu94@163.com>
2019-11-14 17:55:58 +0800

    +1,fix website

---
+++ algo/maxProfit2.js
@@ -0,0 +1,12 @@
+var maxProfit = function (prices) {
+
+  for (let i = 0; i < prices.length; i++) {
+    let inPrice = 0, outPrice = 0;
+    for (let j = i; j < prices.length; j++) {
+
+    }
+  }
+};
+
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]))
