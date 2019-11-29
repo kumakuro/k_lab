f03db046de4bb4e7b9fd91bdd6ab09a16f2f6946
Peggy <maggiegu94@163.com>
2019-11-29 14:04:04 +0800

    调整出场加载条

--- exTest/public/javascripts/panda/panda.js
+++ exTest/public/javascripts/panda/panda.js
@@ -1,8 +1,15 @@
 
-const screenWidth = 2048;
-const screenHeight = 1536;
-const pandaRatio = 0.65;
-var game = new Phaser.Game(screenWidth, screenHeight, Phaser.AUTO, 'phaser-example',
+const screenWidth = 1024;
+const screenHeight = 768;
+const bgRatio = 0.5;
+const pandaRatio = 0.35;
+
+var targetX = screenWidth / 2;
+var stateArr = ['idle', 'walk', 'hunger'];
+var pandaPlayer;
+var bg;
+
+var game = new Phaser.Game(screenWidth, screenHeight, Phaser.AUTO, 'phaserSet',
   {
     preload: preload,
     create: create,
@@ -10,32 +17,27 @@ var game = new Phaser.Game(screenWidth, screenHeight, Phaser.AUTO, 'phaser-examp
   }
 );
 
-var targetX = screenWidth / 2;
-var stateArr = ['idle', 'walk', 'hunger'];
-var pandaPlayer;
-
 function preload() {
   game.load.image('bg', '/javascripts/panda/image/bg1.png')
-
+  // game.load.image('bg', '/javascripts/panda/image/bg0.png')
   game.plugins.add(Fabrique.Plugins.Spine);
   game.load.spine('panda', '/javascripts/panda/shengdanyouniance.json');
 
   var bar = game.add.graphics();
-  bar.beginFill(0x000000, 0.2);
-  bar.drawRect(0, 100, 800, 100);
 
+  game.load.onFileComplete.add(progress => {
+    console.log(progress)
+    bar.beginFill(0xffffff, 1);
+    bar.drawRect(0, screenHeight / 2 - 200, screenWidth * (progress / 100), 100);
+  }, game);
 }
 
 function create() {
-  var bg = game.add.sprite(0, 0, 'bg');
-  pandaPlayer = game.add.spine(targetX, screenHeight / 2 + 600, 'panda');
-  pandaPlayer.scale.x = pandaRatio
-  pandaPlayer.scale.y = pandaRatio
-
-  pandaPlayer.setAnimationByName(0, 'walk', true);
-  pandaPlayer.setSkinByName('2');
-
-  game.input.onDown.add(function () {
+  bg = game.add.sprite(0, 0, 'bg');
+  bg.scale.x = bgRatio
+  bg.scale.y = bgRatio
+  bg.inputEnabled = true;
+  bg.events.onInputDown.add(() => {
     targetX = parseInt(game.input.activePointer.position.x);
     if (targetX > parseInt(pandaPlayer.x)) {
       pandaPlayer.scale.x = pandaRatio
@@ -44,15 +46,23 @@ function create() {
       pandaPlayer.scale.x = -1 * pandaRatio
     }
     pandaPlayer.setToSetupPose();
-  });
+  }, this)
+
+  pandaPlayer = game.add.spine(targetX, screenHeight / 2 + 250, 'panda');
+  pandaPlayer.scale.x = pandaRatio
+  pandaPlayer.scale.y = pandaRatio
+  pandaPlayer.setAnimationByName(0, 'walk', true);
+  pandaPlayer.setSkinByName('2');
+
 }
 
 function update() {
+
   if (targetX > parseInt(pandaPlayer.x)) {
-    pandaPlayer.x += 8;
+    pandaPlayer.x += 8 * bgRatio;
   }
   if (targetX < parseInt(pandaPlayer.x)) {
-    pandaPlayer.x -= 8;
+    pandaPlayer.x -= 8 * bgRatio;
   }
 }
 
