dd5e08a226c81ca106769ad3baeed6cd338884d5
Peggy <maggiegu94@163.com>
2019-12-02 17:35:54 +0800

    setTimeout to delay bind click

--- exTest/public/javascripts/panda/panda.1.js
+++ exTest/public/javascripts/panda/panda.1.js
@@ -7,15 +7,18 @@ const pandaRatio = 0.35;
 var pandaPlayer, game;
 var targetX = screenWidth / 2;
 
-var bgArr = [], bgNum = 0, bgIn = 0; // 0-竹林，1-圣诞
-var clothArr = ['1', '2'], clothIdx = 1; // 1-没衣服，2-圣诞
+var levelNum = 1, levelIn; //1-幼年,2-中年,3-老年
+var bgArr = [], bgNum = 0, bgIn; // 0-竹林，1-圣诞
+var clothArr = ['1', '2'], clothIn; // 1-没衣服，2-圣诞
 var clothClickFlagArr = [null, null]; // 与衣服一一对应，换一套新衣服时，重新绑定点击事件
-var stateArr = ['hunger', 'walk'], stateNum = 1, stateIn = 1;  // 0-饥饿,1-饱腹
+var stateArr = ['hunger', 'walk', 'idle'], stateNum = 1, stateIn;  // 0-饥饿,1-饱腹,2-无聊
 
 
 function preload() {
   game.add.plugin(PhaserSpine.SpinePlugin);
-  game.load.spine('panda', '/javascripts/panda/shengdanyouniance.json');
+  game.load.spine('panda1', '/javascripts/panda/shengdanyouniance.json');
+  game.load.spine('panda2', '/javascripts/panda/shengdanyouniance.json');
+  game.load.spine('panda3', '/javascripts/panda/shengdanyouniance.json');
 
   game.load.image('bg1', '/javascripts/panda/image/bg1.png')
   game.load.image('bg0', '/javascripts/panda/image/bg0.png')
@@ -47,13 +50,16 @@ function create() {
   })
   bgArr.push(bg1)
   bgArr.push(bg0)
+  if (bgNum != bgIn) {
+    bgNum = bgIn;
+  }
   changeBgArr()
 
-  pandaPlayer = game.add.spine(targetX, screenHeight / 2 + 250, 'panda');
+  pandaPlayer = game.add.spine(targetX, screenHeight / 2 + 250, 'panda' + levelNum);
   pandaPlayer.scale.x = pandaRatio
   pandaPlayer.scale.y = pandaRatio
 
-  pandaPlayer.setSkinByName(clothArr[clothIdx]);
+  pandaPlayer.setSkinByName(clothArr[clothIn]);
   pandaPlayer.setToSetupPose();
   bindPandaSkinClick();
   pandaPlayer.setAnimationByName(0, 'walk', true);
@@ -63,6 +69,7 @@ function update() {
   if (targetX > parseInt(pandaPlayer.x)) {
     pandaPlayer.x += 8 * bgRatio;
   }
+
   if (targetX < parseInt(pandaPlayer.x)) {
     pandaPlayer.x -= 8 * bgRatio;
   }
@@ -72,7 +79,7 @@ function update() {
     changeBgArr()
   }
 
-  if (clothClickFlagArr[clothIdx] == false) {
+  if (clothClickFlagArr[clothIn] == false) {
     bindPandaSkinClick();
   }
 }
@@ -102,17 +109,18 @@ function changeBgArr() {
 }
 
 function bindPandaSkinClick() {
-  clothClickFlagArr[clothIdx] = true;
+  clothClickFlagArr[clothIn] = true;
   for (var i in pandaPlayer.children) {
     var itm = pandaPlayer.children[i]
-    console.log(i, '->', itm.children, clothClickFlagArr[clothIdx])
+    console.log(i, '->', itm.children, clothClickFlagArr[clothIn])
     if (itm.children.length > 0) {
       itm.setAll('inputEnabled', true)
       itm.callAll('events.onInputDown.add', 'events.onInputDown', clickPanda)
     } else {
-      clothClickFlagArr[clothIdx] = false;
+      clothClickFlagArr[clothIn] = false;
     }
   }
+  console.log('clothClickFlagArr->', clothClickFlagArr)
 }
 
 function clickPanda() {
@@ -120,8 +128,14 @@ function clickPanda() {
   return false;
 }
 
-function initPanda(data) {
-  console.log('initPanda->', data)
+function initPanda({ level = 1, state = 1, cloth = 1, bg = 1 }) {
+  console.log('initPanda->', level, state, cloth, bg)
+
+  levelIn = level
+  stateIn = state
+  clothIn = cloth
+  bgIn = bg
+
   game = new Phaser.Game(screenWidth, screenHeight, Phaser.AUTO, 'phaserSet',
     {
       preload: preload,
@@ -132,10 +146,13 @@ function initPanda(data) {
 }
 
 function changeCloth(num) {
-  clothIdx = clothArr.indexOf(num);
+  clothIn = clothArr.indexOf(num);
   pandaPlayer.setSkinByName(num);
   pandaPlayer.setToSetupPose();
-  bindPandaSkinClick();
+  setTimeout(() => {
+    bindPandaSkinClick();
+  }, 300)
+
 }
 
 function changeBg(num) {
